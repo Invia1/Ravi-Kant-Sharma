6b0645e20b0b9fa49648d85da156be5ec45b3dae
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-17 14:01:39 +0530

    Add files via upload

---
+++ FootBall-team-app/FootBall-team-app/Model/PlayersDetail.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FootBall_team_app.Model
+{
+     class PlayersDetail
+    {
+        public int playerNo { get; set; }
+        public string playerName { get; set; }
+        public string playerPosition { get; set; }
+    }
+}
